5a842feb11be1c7ad69c5afc0da25ebf1d3747ee
conanzhangxin <conanzhangxin@gmail.com>
2013-07-22 19:11:04 +0800

    delete idea files and add constants

---
+++ src/main/java/com/code/project/biz/constants/Constants.java
@@ -0,0 +1,16 @@
+package com.code.project.biz.constants;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xinnan.zx
+ * Date: 13-7-22
+ * Time: 下午3:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class Constants {
+
+    public static final int NO_RIGHT = 0;
+
+    public static final int HAS_RIGHT = 1;
+
+}
